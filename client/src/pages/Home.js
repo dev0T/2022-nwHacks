1673f3175024436899fc75a80c17262af8551b17
Cezaraus <brianopao@gmail.com>
2022-01-15 19:51:05 -0800

    Feature added inital Home page component

---
+++ client/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavbarComponent from '../components/Navbar'
+
+export default function Home() {
+    return (
+        <div>
+            <NavbarComponent/>
+        </div>
+    )
+}
