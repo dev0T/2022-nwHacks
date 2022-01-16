2638856e809489850c18c6b739e894f3f10cfb2a
Teryn <terynalves@gmail.com>
2022-01-15 21:53:37 -0800

    [feat] added filter by classification end point

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const { getByClassification } = require("./src/service.js");
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -9,9 +10,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/ping", (req, res) => {
-  res.send("pong");
+  res.send("pong, server is alive!");
 });
 
+app.post("/charities/:classification", async (req, res) => {
+  try {
+    const charities = await getByClassification(req.params.classification);
+    res.send(charities);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 
 app.use(express.static(path.join(__dirname, "client/public")));
