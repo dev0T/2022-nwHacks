cf3c1408301fd1e20b26b494a227a5e04eed5710
Cezaraus <brianopao@gmail.com>
2022-01-15 20:05:13 -0800

    Update initial layout with proper components

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,12 +2,10 @@ import React from 'react';
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
 
 export default function NavbarComponent() {
     return (
-        <Container align="center">
             <Navbar bg="light" variant="light">
                 <Container>
                     <Navbar.Brand href="/">VanDons</Navbar.Brand>
@@ -18,19 +16,5 @@ export default function NavbarComponent() {
                     </Nav>
                 </Container>
             </Navbar>
-            <Container>
-                Welcome to VanDons!
-            </Container>
-            <Container>
-                <Row>
-                    <Col>
-                        Pane 1
-                    </Col>
-                    <Col>
-                        Pane 2
-                    </Col>
-                </Row>
-            </Container>
-        </Container>
     )
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import NavbarComponent from '../components/Navbar'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Carousel from 'react-bootstrap/Carousel';
+import Container from 'react-bootstrap/Container';
+
 
 export default function Home() {
     return (
         <div>
-            <NavbarComponent/>
+        <NavbarComponent/>
+        <Container align="center">
+            <Container>
+                Welcome to VanDons!
+            </Container>
+            <Container>
+                <Row>
+                    <Col>
+                        Pane 1
+                    </Col>
+                    <Col>
+                        Pane 2
+                    </Col>
+                </Row>
+            </Container>
+        </Container>
         </div>
     )
 }
