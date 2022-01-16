21e81fc2c859c03515f5851ec6ccf5da5f71fd47
Cezaraus <brianopao@gmail.com>
2022-01-16 06:42:52 -0800

    Update display with charity list

--- client/src/pages/Discover.js
+++ client/src/pages/Discover.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Container from 'react-bootstrap/esm/Container';
 import NavbarComponent from '../components/Navbar';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './styles.css';
 import Map from '../components/Map'
+import axios from 'axios';
+import {nanoid} from 'nanoid';
 
 export default function Discover() {      
+    const [charityState, setCharity] = useState([""]);
+
+    useEffect(() => {
+        axios.post("/charities").then((response) => {
+            setCharity(response.data);
+        });
+    }, []);
+
+    function displayCharity(){
+        let charity = [];
+        charity = charityState.map(x=><li key={nanoid()}>
+           <h5>{x.name}</h5> 
+            <ul><li>{x.description}</li></ul>
+        </li>);
+
+        return charity;
+    }
 
     return (
         <div>
             <NavbarComponent/>
-            <div className="top-height">
-                <Container align="center">
-                    <Row height="300px">
-                        <Col>1 of 3
-                            <br></br>
-                        </Col>
-                        <Col>2 of 3</Col>
-                        <Col>3 of 3</Col>
-                    </Row>
-                </Container>
+            <div className="top-height overflow">
+                { displayCharity()}
             </div>
             <Map/>
         </div>
